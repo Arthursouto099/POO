49a808c6970bc968be9f88f6b6e01cb0040e0fa5
Arthursouto099 <arthurtavaressouto@gmail.com>
2024-12-13 12:54:34 -0300

    update

--- exercicio_classes_abstratas/index.ts
+++ exercicio_classes_abstratas/index.ts
@@ -45,14 +45,14 @@ abstract class Account {
     }
 
 
-    abstract withdraw(valueToWitdraw: number): number
+    abstract withdraw(valueToWitdraw: number, nameInvestment?: string): number
 
 
     transferMoney(account: Account, value: number) {
         if (value > this.balance) {
             console.log('Salado insuficiente')
         }
-        else if (value > 4.99999) {
+        else if (value > 499999) {
             console.log("Limite de Tranferencia atingido.")
         }
 
@@ -80,13 +80,13 @@ class AccountInvestment extends Account {
     }
 
 
-    makeInvestment(nameInstment: string, valueInvested: number, dateInvestment: Date, risk: 'alto' | 'medio' | 'baixo', typeInvestment: string, timeInYears: number) {
+    makeInvestment(nameInvestment: string, valueInvested: number, dateInvestment: Date, risk: 'alto' | 'medio' | 'baixo', typeInvestment: string, timeInYears: number) {
         if (valueInvested > this.getBalance()) {
             console.log("Você não possui saldo suficiente")
             return 'Investimento não realizado'
         }
         this.setBalance(this.getBalance() - valueInvested)
-        const newInvestment = new Investment(nameInstment, valueInvested, dateInvestment, risk, typeInvestment, timeInYears)
+        const newInvestment = new Investment(nameInvestment, valueInvested, dateInvestment, risk, typeInvestment, timeInYears)
         this.investments.push(newInvestment)
         return "Investimento realizado com sucesso"
     }
@@ -112,7 +112,6 @@ class AccountInvestment extends Account {
         }
 
         const roi: number = ((invested - valueInvested) / valueInvested) * 100
-        console.log(`retorno {ROI} = ${roi}%`)
         return roi
     }
 
@@ -121,10 +120,76 @@ class AccountInvestment extends Account {
 
 
 
-    withdraw(valueToWitdraw: number): number {
-        return 0
+    withdraw(valueToWitdraw: number, nameInvestment: string): number {
+      this.investments.forEach(investment => {
+        if(investment.nameInvestment === nameInvestment) {
+            if(valueToWitdraw > investment.valueInvested) {
+                console.log("Saldo insuficiente")
+                return 0
+            }
+            
+            console.log(`${investment.valueInvested}$, Valor antes do saque`)
+            investment.valueInvested -= valueToWitdraw
+            console.log(`o valor do investimento ${investment.nameInvestment} atual é de ${investment.valueInvested}$ investidos`)
+            return 1
+        }
+      })
+
+      console.log("Nome não encontrado")
+      
+      return 0
+      
     }
 
+}
+
+class Savings extends Account {
+    private _saivings: Investment | null = null
+    constructor(user: User, typeAccount: TypesAccounts) {
+        super(user, typeAccount)
+    }
+    
+    createSavings(valueInvested: number, dateInvestment: Date, timeInYears: number) {
+        const newInvestment = new Investment("Poupança", valueInvested, dateInvestment, 'baixo', 'Poupança', timeInYears )
+        this._saivings = newInvestment
+    }
+
+    addSavings(value: number): number {
+        if(value > this.getBalance()) {
+            console.log("Operação falhou, saldo insuficiente")
+            return 0
+        }
+        if(this._saivings !== null) {
+            this._saivings.valueInvested += value
+            console.log("Operação feita com sucesso")
+            return 1
+        }
+        return 0
+     
+    }
+
+    getSavings() {
+      return this._saivings
+     
+
+    }
+
+    withdraw(valueToWitdraw: number): number {
+        if(valueToWitdraw > this.getBalance()) {
+            console.log("Saldo insuficiente")
+            return 0
+        }
+
+        if(this._saivings !== null) {
+            this._saivings.valueInvested -= valueToWitdraw
+            console.log("Sucesso na operação")
+            console.log(`O saldo atual da poupança é de ${this._saivings.valueInvested}`)
+            return 1
+        }
+
+        return 0
+   
+    }
 }
 
 
@@ -156,15 +221,15 @@ class AccountCurrent extends Account {
 
 
 class Investment {
-    nameInvstment: string = ''
+    nameInvestment: string = ''
     valueInvested: number = 0
     dateInvestment: Date
     typeInvestment: string | null = null
     risk: 'alto' | 'medio' | 'baixo' = 'alto'
     timeInYears: number
 
-    constructor(nameInstment: string, valueInvested: number, dateInvestment: Date, risk: 'alto' | 'medio' | 'baixo', typeInvestment: string, timeInYears: number) {
-        this.nameInvstment = nameInstment
+    constructor(nameInvestment: string, valueInvested: number, dateInvestment: Date, risk: 'alto' | 'medio' | 'baixo', typeInvestment: string, timeInYears: number) {
+        this.nameInvestment = nameInvestment
         this.valueInvested = valueInvested
         this.dateInvestment = dateInvestment
         this.typeInvestment = typeInvestment
@@ -203,9 +268,26 @@ class User extends Person {
 }
 
 const newUser = new User('tavares', 16, '11111.1111.1', null)
+const newUser2 = new User('vitor', 23, '1881010111', null)
+const newUser3 = new User("julia", 17, '896329869972', null)
 const c1 = new AccountInvestment(newUser, TypesAccounts.Investment)
+const c2 = new AccountCurrent(newUser2, TypesAccounts.Current)
+const c3 = new Savings(newUser3, TypesAccounts.Savings)
 
-c1.addBalance(4000)
-c1.makeInvestment("Imobiliaria", 1200, new Date(), "alto", "Imobiliario", 4)
-c1.calculateRoi()
 
+// c1.addBalance(80000)
+// c1.makeInvestment("Imobiliaria", 1200, new Date(), "alto", "Imobiliario", 4)
+// c1.makeInvestment("Tesouro selic", 4000, new Date(), 'medio', 'Tesouro', 6)
+// c1.withdraw(1000, 'Imobiliaria')
+// c1.calculateRoi()
+
+c2.addBalance(5000) 
+c2.transferMoney(c1, 1000)
+console.log(c1)
+
+c3.addBalance(500000)
+c3.createSavings(40000, new Date(), 76)
+c3.addSavings(100)
+
+
+console.log(c3.getSavings())
